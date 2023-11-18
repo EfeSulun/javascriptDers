dd01924206765bfa5e9edad6e8ac7ddc5d4b43cc
efe <efekasar074@gmail.com>
2023-11-18 16:44:23 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,129 @@
+// console.log("Merhaba");
+// console.log(1234);
+// console.log(true);
+// console.log([1,2,3,]);
+// console.error("hata oluştu");
+// console.warn("bir uyarı oluştu");
+
+
+
+
+// var age;
+// console.log(age);
+
+
+// age=20;
+// console.log(age);
+
+
+
+// var fullname='Efe Sülün';
+// console.log(fullname);
+
+
+
+// fullname='Hakan Çalhanoğlu';
+// console.log(fullname);
+
+
+// var yas1;
+// var _yas2;
+// var $yas3;
+
+// var ad_soyad='Efe Sülün';
+// var adSoyad='Zlatan İbrahimovic';
+
+
+
+// var Firstname='Efe';
+// var firstname='Mustafa';
+// // var , let , const
+
+
+// let city='İstanbul';
+// console.log(city);
+
+
+// const email='abc@gmail.com';
+// console.log(email);
+// değişkenler
+
+
+
+
+let age= 20;
+
+
+let money=100.5;
+
+
+let isActive=false;
+
+
+let job=null;
+ 
+
+let car;
+console.log(typeof car);
+
+
+let names=['Ali','Ahmet','Can'];
+
+let adress= {
+    city:'Kocaeli',
+    country:'Türkiye'
+}
+ 
+
+
+
+
+var calculateAge =function(){
+ return 0;   
+}
+
+console.log (typeof calculateAge);
+
+
+
+
+
+alert('Merhaba');
+
+
+
+
+let val;
+
+
+val= String(10);
+
+
+val=new Date();
+
+
+
+
+
+
+console.log(val);
+console.log(typeof val);
+console.log(val.length);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
